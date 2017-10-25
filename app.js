d3e340786c02233c56438e6da10592e400c996fa
Phoebe <meipbe@gmail.com>
2017-10-25 11:44:26 -0700

    Completed

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// TODO
+const App = () => (
+  <div>
+  <h2>My Grocery List</h2>
+  <GroceryList items={[1, 2, 3]} />
+  </div>
+);
+
+const GroceryList = (props) => (
+
+  // let clickMe = (event) => {
+  //   console.log('clickie');
+  // };
+
+  <ul>
+  {props.items.map(item =>
+    <GroceryListItem item={item} />
+  )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  clickie() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    let style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.done ? 'bold': 'none'
+    };
+
+    return(
+      <li style={style} onClick={this.clickie.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
